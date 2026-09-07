c1b22014397c2a972c8eba37952b5d3bfb6a6662
jangsang1214 <jangsang051214@gmail.com>
2026-09-08 04:21:13 +0900

    Refine GARANG navigation and wordmark

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
+import { BrandMark } from "@/components/BrandMark";
 
 export function Navigation() {
   return (
     <nav className="nav shell" aria-label="Primary navigation">
-      <Link href="/" className="nav__brand serif">GARANG</Link>
+      <Link href="/" className="nav__brand"><BrandMark compact /></Link>
       <div className="nav__links">
         <Link className="nav__link" href="/origin">Origin</Link>
-        <Link className="nav__link" href="/objects/gogok-001">Objects</Link>
+        <Link className="nav__link" href="/objects/gogok-001">Object 001</Link>
         <Link className="nav__link" href="/archive">Archive</Link>
-        <Link className="nav__link nav__acquire" href="/objects/gogok-001#acquire">Acquire</Link>
+        <Link className="nav__link nav__acquire" href="/objects/gogok-001#acquire">Private acquisition</Link>
       </div>
     </nav>
   );
